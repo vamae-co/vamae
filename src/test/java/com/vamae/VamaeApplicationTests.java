3428bb54824becba2ed8acd9a19876af30cae7df
Vladyslav <v1adem@ukr.net>
2024-03-01 13:23:10 +0200

    init commit

---
+++ src/test/java/com/vamae/VamaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vamae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VamaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
