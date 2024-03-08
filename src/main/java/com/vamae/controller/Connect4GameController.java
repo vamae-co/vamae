09827823593913b993a8994b7cdddeeecabad8c0
Dykalo Andrij <dikaloandrij@gmail.com>
2024-03-08 16:59:01 +0200

    Added init endpoint

---
+++ src/main/java/com/vamae/controller/Connect4GameController.java
@@ -0,0 +1,25 @@
+package com.vamae.controller;
+
+import com.vamae.entity.Connect4Game;
+import com.vamae.service.Connect4GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/connect4")
+public class Connect4GameController {
+    private final Connect4GameService connect4GameService;
+
+    @GetMapping
+    public Connect4Game initGame(
+            @RequestParam(required = false, defaultValue = "7") int columns,
+            @RequestParam(required = false, defaultValue = "6") int rows,
+            @RequestParam Long firstPlayerId,
+            @RequestParam Long secondPlayerId,
+            @RequestParam Double betSum
+    ) {
+        System.out.println("CONTROLLER GAME INITIALIZATION");
+        return connect4GameService.init(columns, rows, firstPlayerId, secondPlayerId, betSum);
+    }
+}
