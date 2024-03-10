50f3403458873f1086c0b97cc1e1579b4a65fce4
Dykalo Andrij <dikaloandrij@gmail.com>
2024-03-10 18:32:01 +0200

    Added request and response entities

---
+++ src/main/java/com/vamae/authorization/payload/response/LoginResponse.java
@@ -0,0 +1,11 @@
+package com.vamae.authorization.payload.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class LoginResponse {
+    private String token;
+    private String userName;
+}
