1b57e14843728b1ed0bc56a5d024bc6dd0e80837
Dykalo Andrij <dikaloandrij@gmail.com>
2024-03-10 14:34:57 +0200

    Modified User class and pom.xml

--- src/main/java/com/vamae/model/User.java
+++ src/main/java/com/vamae/model/User.java
@@ -1,20 +1,23 @@
 package com.vamae.model;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Entity
-@NoArgsConstructor
+@Builder
+@Document("users")
 public class User {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     private String id;
     @NotEmpty(message = "Username is required")
     private String username;
+    @NotEmpty(message = "Password is required")
+    private String password;
+    private boolean isActive;
 }
