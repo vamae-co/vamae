59416ea5f39d8f8b8ba897d78190f5dbd0ebb657
Dykalo Andrij <dikaloandrij@gmail.com>
2024-03-12 13:37:39 +0200

    Moved secret key to secrets.properties file and exclude it in .gitignore file

--- src/main/java/com/vamae/authorization/security/config/JwtService.java
+++ src/main/java/com/vamae/authorization/security/config/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,7 @@ import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
+@PropertySource("classpath:secrets.properties")
 public class JwtService {
 
     @Value("${application.secrets.key}")
