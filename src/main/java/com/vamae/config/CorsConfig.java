0c4e5b758b9c6c9177c47552fb6688e400683fce
Makarov Andrii <makarov.andrii.10@gmail.com>
2024-03-17 16:36:34 +0200

    add origin for CORS

--- src/main/java/com/vamae/config/CorsConfig.java
+++ src/main/java/com/vamae/config/CorsConfig.java
@@ -9,6 +9,6 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
     }
 }
