c43a8497a711008a29266362148a840d27f57001
Dykalo Andrij <dikaloandrij@gmail.com>
2024-03-21 11:05:11 +0200

    Removed @Getter from GameBoard record

--- src/main/java/com/vamae/connect4/lib/entity/GameBoard.java
+++ src/main/java/com/vamae/connect4/lib/entity/GameBoard.java
@@ -1,6 +1,5 @@
 package com.vamae.connect4.lib.entity;
 
-import lombok.Getter;
 import com.vamae.connect4.lib.enums.Piece;
 
 import java.util.List;
@@ -8,7 +7,6 @@ import java.util.List;
 /**
  * Class that represents game board
  */
-@Getter
 public record GameBoard(
         List<List<Piece>> columns,
         int rows
