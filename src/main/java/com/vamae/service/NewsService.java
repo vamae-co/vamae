e7441c2a3b3d4f3e1285a9825320362c3fc37ada
Vladyslav <v1adem@ukr.net>
2024-03-22 17:11:20 +0200

    Added NewsService

---
+++ src/main/java/com/vamae/service/NewsService.java
@@ -0,0 +1,41 @@
+package com.vamae.service;
+
+import com.vamae.entity.News;
+import com.vamae.entity.Tag;
+import com.vamae.payload.request.CreateNewsRequest;
+import com.vamae.repository.NewsRepository;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class NewsService {
+
+    private final NewsRepository newsRepository;
+
+    public News getNewsById(String id){
+        return newsRepository.findNewsById(id)
+                .orElseThrow(() -> new NoSuchElementException("News not found!"));
+    }
+
+    public List<News> getAllNews() {
+        return newsRepository.findAll();
+    }
+
+    public void deleteNewsById(String id) {
+        newsRepository.deleteById(id);
+    }
+
+    public News createNews(@NotNull CreateNewsRequest request) {
+        return News.builder()
+                .date(request.date())
+                .content(request.content())
+                .imageUrl(request.imageUrl())
+                .tags(request.tags())
+                .build();
+    }
+}
