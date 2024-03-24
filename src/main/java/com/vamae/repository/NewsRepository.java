10c0c0c9567d17585a7a751ff24da07aaa882797
Vladyslav <v1adem@ukr.net>
2024-03-24 13:21:03 +0200

    Removed unused imports

--- src/main/java/com/vamae/repository/NewsRepository.java
+++ src/main/java/com/vamae/repository/NewsRepository.java
@@ -1,12 +1,8 @@
 package com.vamae.repository;
 
 import com.vamae.entity.News;
-import lombok.NonNull;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface NewsRepository extends MongoRepository<News, String> {
