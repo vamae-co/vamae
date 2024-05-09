f4fe63cc7eccd83c850c5e6a740f60f77f90a93a
MihaVar <82413122+MihaVar@users.noreply.github.com>
2024-05-09 18:07:17 +0300

    minor style fix

--- src/main/java/com/vamae/statistic/repository/StatisticRepository.java
+++ src/main/java/com/vamae/statistic/repository/StatisticRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface StatisticRepository extends MongoRepository<Statistic, String> {
+
     Optional<Statistic> findByUsername(String username);
 }
