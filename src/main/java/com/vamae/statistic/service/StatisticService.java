a8409bbff6a4ea4d382ed2ab20dcacfb0bd88604
MihaVar <82413122+MihaVar@users.noreply.github.com>
2024-05-09 11:01:30 +0300

    minor bugfix

--- src/main/java/com/vamae/statistic/service/StatisticService.java
+++ src/main/java/com/vamae/statistic/service/StatisticService.java
@@ -12,49 +12,34 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class StatisticService {
+
     private final StatisticRepository statisticRepository;
+
+    public StatisticResponse getStatistic(String username) {
+        Statistic statistic = statisticRepository.findByUsername(username)
+                .orElseThrow(
+                        () -> new IllegalArgumentException(String.format("Statistic with username %s not found", username))
+                );
+        return new StatisticResponse(username, statistic.getAuthCount());
+    }
 
     @Transactional
     public StatisticResponse updateAuthCount(String username) {
         Optional<Statistic> statistic = statisticRepository.findByUsername(username);
 
         if (statistic.isPresent()) {
-            Statistic presentStatistic = statistic.get();
-            int authCount = presentStatistic.getAuthCount();
-            statisticRepository.delete(presentStatistic);
-            Statistic newStatistic = new Statistic(username, authCount + 1);
-            statisticRepository.save(newStatistic);
-            return new StatisticResponse(username, newStatistic.getAuthCount());
-        }
-        else {
+            int authTimes = statistic.get().getAuthCount();
+            if (authTimes == 0) {
+                statistic.get().setAuthCount(1);
+                statisticRepository.save(statistic.get());
+            } else {
+                statistic.get().setAuthCount(authTimes + 1);
+                statisticRepository.save(statistic.get());
+            }
+            return new StatisticResponse(username, statistic.get().getAuthCount());
+        } else {
             Statistic newStatistic = statisticRepository.save(new Statistic(username, 1));
             return new StatisticResponse(username, newStatistic.getAuthCount());
         }
     }
-    public StatisticResponse getStatistic(String username) {
-        Statistic statistic = statisticRepository.findByUsername(username).orElseThrow(() -> new IllegalArgumentException(String.format("Statistic with username %s not found", username)));
-        return new StatisticResponse(username, statistic.getAuthCount());
-    }
-
-    //    Method with updating mongodb value (doesn't work)
-    //    public StatisticResponse updateAuthTimes(String username) {
-    //        Optional<Statistic> statistic = statisticRepository.findByUsername(username);
-    //
-    //        if (statistic.isPresent()) {
-    //            int authTimes = statistic.get().getAuthTimes();
-    //            if(authTimes == 0) {
-    //                statistic.get().setAuthTimes(1);
-    //                statisticRepository.save(statistic.get());
-    //            }
-    //            else {
-    //                statistic.get().setAuthTimes(authTimes + 1);
-    //                statisticRepository.save(statistic.get());
-    //            }
-    //            return new StatisticResponse(username, statistic.get().getAuthTimes());
-    //        }
-    //        else {
-    //           Statistic newStatistic = statisticRepository.save(new Statistic(username, 1));
-    //           return new StatisticResponse(username, newStatistic.getAuthTimes());
-    //        }
-    //    }
 }
