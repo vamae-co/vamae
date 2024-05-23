b6918bf025d74ef5af523c51f470bd80cdccb65e
Makarov Andrii <makarov.andrii.10@gmail.com>
2024-05-23 18:54:04 +0300

    fix message brokers/add authorization check

--- src/main/java/com/vamae/config/WebSocketConfiguration.java
+++ src/main/java/com/vamae/config/WebSocketConfiguration.java
@@ -1,23 +1,77 @@
 package com.vamae.config;
 
+import com.vamae.authorization.security.config.JwtService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("http://127.0.0.1:5500").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/topic", "/user");
         registry.setApplicationDestinationPrefixes("/poker", "/connect4");
-        registry.setUserDestinationPrefix("/topic");
+        registry.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(@NonNull Message<?> message, @NonNull MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (accessor == null) {
+                    return null;
+                }
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    List<String> tokenList = accessor.getNativeHeader("Authorization");
+                    if (tokenList != null && !tokenList.isEmpty()) {
+                        String token = tokenList.getFirst();
+                        if (token.startsWith("Bearer ")) {
+                            token = token.substring(7);
+                            String username = jwtService.extractUsername(token);
+                            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                            if (jwtService.isTokenValid(token, userDetails)) {
+                                Authentication auth = new UsernamePasswordAuthenticationToken(
+                                        userDetails,
+                                        null,
+                                        userDetails.getAuthorities()
+                                );
+                                accessor.setUser(auth);
+                            }
+                        }
+                    }
+                }
+                return message;
+            }
+        });
     }
 }
